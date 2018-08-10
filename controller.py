06f0db4578bae6b0ac8fc360981c101cb93f3e17
Mykola <mykola99ds@gmail.com>
2018-08-10 23:18:59 +0300

    Remove images, change screenshot saver

--- controller.py
+++ controller.py
@@ -1,15 +1,16 @@
-from screenshoter import shot
+import screenshoter as ss
 from bots.crazy_bot import CrazyBot
 from game import Game
 from image_parser import field
 import time
 
 how_long = 10
+shooter = ss.ScreenShooter()
 bot = CrazyBot()
 with Game() as game:
     start = time.time()
     while time.time() - start < how_long:
-        image = shot()
+        image = shooter.get_screen()
         parsed = field(image)
         action = bot.get_action(parsed)
         action()
